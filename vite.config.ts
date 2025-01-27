fd84215495cf1bcfacc406e29172ff71c17c6f25
Razdolbyte <lighterror1@mail.ru>
2025-01-27 11:52:31 +0300

    create vue project and add packages

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import stylelint from "vite-plugin-stylelint";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+    stylelint({
+      dev: true,
+      fix: true,
+      include: "**/*.{css,scss,sass,vue}",
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
